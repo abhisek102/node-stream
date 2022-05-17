bd95904df94af425b61b036c64a09c6b9f203a0c
abhisek102 <abhisek.nanda@contractor.tatacommunications.com>
2022-05-17 13:30:12 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   const fs = require('fs');
+  // one way
+  // const rs = fs.createReadStream('test.txt');
+  // rs.on('data', (chunkdata) => {
+  //   res.write(chunkdata);
+  // });
+  // rs.on('end', () => {
+  //   res.end('data readed succesfully');
+  // });
+  // rs.on('error', (err) => {
+  //   console.log(err);
+  //   res.end('file not found');
+  // });
+  // other way
   const rs = fs.createReadStream('test.txt');
-  rs.on('data', (chunkdata) => {
-    res.write(chunkdata);
-  });
-  rs.on('end', () => {
-    res.end('data readed succesfully');
-  });
-  rs.on('error', (err) => {
-    console.log(err);
-    res.end('file not found');
-  });
+  rs.pipe(res);
 });
 
 server.listen(8000);
